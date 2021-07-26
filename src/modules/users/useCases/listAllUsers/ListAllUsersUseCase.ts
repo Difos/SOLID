fbf6734dc4283968ddfb940d2f74ff471614092e
wesleyepeneto <wesley.epeneto@vale.com>
2021-07-26 00:43:40 -0300

    enviando solid teste

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,8 +9,22 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    
+    const user = this.usersRepository.findById(user_id);
+
+    if(!user){
+      throw new Error("Mensagem do erro");
+    }
+
+    if(!user.admin){
+      throw new Error("Mensagem do erro");
+    }
+
+    return this.usersRepository.list();
+
   }
+
+  
 }
 
 export { ListAllUsersUseCase };
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,19 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const {user_id} = request.params;
+
+   
+
+    try {
+      const all = this.turnUserAdminUseCase.execute({user_id});
+
+      return response.status(200).json(all); 
+    } catch (error) {
+      return response.status(404).json({error});
+    }
+    
+
   }
 }
 
